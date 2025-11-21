2ec85e45309d9cb8e136d0c80ae2d99e3d9b6263
colonelvoid <parthnaik.office@gmail.com>
2025-11-21 19:02:54 +0530

    Initial commit

---
+++ blackjack/blackjack.js
@@ -0,0 +1,38 @@
+let firstCard = 10;
+let secondCard = 4;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+
+function startGame() 
+{
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+    sumEl.textContent = "Sum: " + sum;
+
+    if (sum <= 20)
+    {
+        message = "Do you want to draw a new card? 😀";
+    }
+    else if (sum === 21) 
+    {
+        message = "Wohoo! You have got BlackJack! 🥳";
+        hasBlackJack = true;
+    } 
+    else 
+    {
+        message = "You're out of the game 😭";
+        isAlive = false;
+    }
+
+    messageEl.textContent = message;
+}
+
+function newCard()
+{
+    
+}
